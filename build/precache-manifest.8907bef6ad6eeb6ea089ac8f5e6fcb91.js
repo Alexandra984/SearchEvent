2ac39ee5a60c4fad086f041fadb70945d195fbe8
Alexandra Grecu <alexandragrecu9844@gmail.com>
2020-05-28 15:43:24 +0300

    final commit.

--- build/precache-manifest.8907bef6ad6eeb6ea089ac8f5e6fcb91.js
+++ build/precache-manifest.8907bef6ad6eeb6ea089ac8f5e6fcb91.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6dbbbb778d3110a1051f1a788cf1bdbd",
+    "revision": "0edd32b36659d15cfd4f05a99144bfda",
     "url": "/index.html"
   },
   {
-    "revision": "fcd2d0fa7bae484dc431",
+    "revision": "2122ed94f9633b3561d9",
     "url": "/static/css/main.3bb2f9d8.chunk.css"
   },
   {
-    "revision": "55c32482e005463b4f6e",
-    "url": "/static/js/2.0b3ba310.chunk.js"
+    "revision": "0a57c208d661d73f866e",
+    "url": "/static/js/2.2b672f99.chunk.js"
   },
   {
     "revision": "4d323e9b06b56bc96b0544f5ef818a27",
-    "url": "/static/js/2.0b3ba310.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.2b672f99.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fcd2d0fa7bae484dc431",
-    "url": "/static/js/main.7fe1af06.chunk.js"
+    "revision": "2122ed94f9633b3561d9",
+    "url": "/static/js/main.eff36df3.chunk.js"
   },
   {
     "revision": "68a9665f6992de0c4239",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b350249c8a4a0f60745bc9670573cef6.js"
+  "/precache-manifest.8907bef6ad6eeb6ea089ac8f5e6fcb91.js"
 );
 
 self.addEventListener('message', (event) => {
